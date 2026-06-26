be4f0c95b7f9bc3f7b09260ac5c681593eec2fb0
ShradhaPatil <patil.shradha0317@gmail.com>
2026-06-26 12:07:30 +0530

    Initial project setup

---
+++ src/main/java/com/shradha/smart_hospital_managment/SmartHospitalManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.shradha.smart_hospital_managment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartHospitalManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartHospitalManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shradha/smart_hospital_managment/SmartHospitalManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shradha.smart_hospital_managment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHospitalManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
